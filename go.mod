3f524d8a39fa6b6a9184905f4b22eb43a9713c7c
Solomon Schuster <90484793+Solly922@users.noreply.github.com>
2023-09-14 22:18:58 -0700

    adds router and cors

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/solly922/rss-agg
 
 go 1.21.0
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/go-chi/cors v1.2.1
--- main.go
+++ main.go
@@ -3,19 +3,42 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello World!")
-
 	godotenv.Load(".env")
-	
+
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           3000,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Println("Port: ", portString)
