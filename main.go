fd2b469f9007bc5a200b1bdf65e17c9d058788c2
Solomon Schuster <90484793+Solly922@users.noreply.github.com>
2023-09-17 21:22:04 -0700

    allows users to follow feed

--- main.go
+++ main.go
@@ -61,6 +61,8 @@ func main() {
 	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
 	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 
+	v1Router.Post("/feed-follows", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
+
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
