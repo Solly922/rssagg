31520a08f674881307332041efd113789986bb58
Solomon Schuster <90484793+Solly922@users.noreply.github.com>
2023-09-17 18:04:22 -0700

    gets users with api keys

--- handler_user.go
+++ handler_user.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/solly922/rss-agg/internal/auth"
 	"github.com/solly922/rss-agg/internal/database"
 )
 
@@ -35,5 +36,22 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
+		return
+	}
+
 	respondWithJSON(w, 200, databaseUserToUser(user))
+
 }
